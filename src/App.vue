70a768f56190b8c84a5669a012c14c09851d0ec1
Jacek <ja_cek@gmx.com>
2021-05-28 14:13:33 +0200

    project update

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,28 @@
   <v-app id="inspire">
     <v-navigation-drawer
       v-model="drawer"
+      :mobile-breakpoint="768"
       app
     >
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Vuetify Todo
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Best Todo Ever!
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
+      <v-img
+        class="pa-4 pt-7"
+        src="mountains.jpg"
+        height="170"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      >
+        <v-avatar size="70" class="mb-2">
+          <img
+            src="avatar.png"
+            alt="Avatar"
+          >
+        </v-avatar>
+        <div class="white--text text-subtitle-1 font-weight-bold">
+          Jacek Czajkowski
+        </div>
+        <div class="white--text text-subtitle-2">
+          ja_cek
+        </div>
+      </v-img>
 
       <v-list
         dense
@@ -44,6 +52,7 @@
       dark
       src="mountains.jpg"
       prominent
+      height="170"
     >
       <template v-slot:img="{ props }">
         <v-img
@@ -52,14 +61,17 @@
         ></v-img>
       </template>
 
-      <v-container class="pa-0">
+      <v-container class=" header-container pa-0">
         <v-row>
           <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
           <v-spacer></v-spacer>
           <search />
         </v-row>
         <v-row>
-          <v-toolbar-title class="ml-4">Vuetify Todo</v-toolbar-title>
+          <v-toolbar-title class="text-h4 ml-4">Vuetify Todo</v-toolbar-title>
+        </v-row>
+        <v-row>
+          <live-date-time />
         </v-row>
       </v-container>
 
@@ -83,7 +95,13 @@
     }),
     components: {
       'search': require('@/components/Tools/Search.vue').default,
+      'live-date-time': require('@/components/Tools/LiveDateTime.vue').default,
       'snackbar': require('@/components/Shared/Snackbar.vue').default
     }
   }
 </script>
+
+<style lang="sass">
+  .header-container
+    max-width: none !important
+</style>
--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -45,7 +45,7 @@ export default {
   props: ['task'],
   filters: {
     niceDate(value) {
-      return format(new Date(value), 'MMM d')
+      return format(new Date(value), 'd.MM')
     }
   },
   components: {
