614c52dd66ad4e1226a3c574fb58f3e4fb271257
Jacek <ja_cek@gmx.com>
2021-06-01 01:21:38 +0200

    project update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about pa-6">
     <h1>{{ $store.state.appTitle }}</h1>
-    <p>Ogarnij się to bardzo prosta i darmowa aplikacja do zarządzania zadaniami, która pomoże Ci utrzymać porządek każdego dnia. Ta apka może Ci pomóc w organizacji, nieważne kim jesteś, ani co robisz! Rozpocznij od razu, szybko i łatwo dodając nowe zadania używając szybkiego dodawania "+"</p>
+    <p>Jest to bardzo prosta i darmowa aplikacja do zarządzania swoimi codziennymi zadaniami. Ogarnij się pomoże Ci utrzymać porządek każdego dnia. Ta apka pomoże w organizacji jakichkolwiek zadań, nieważne kim jesteś, ani co robisz! Rozpocznij od razu, szybko i łatwo dodając nowe zadania</p>
+    <h3>Instrukcja obsługi</h3>
+    <p>1. Aby dodać pierwszą i kolejne pozycje naciśnij pod datą na pole z napisem dodaj zadanie.</p>
+    <p>2. Następnie wpisz zadanie i potwierdź znakiem + lub na klawiaturze klawiszem enter/return.</p>
+    <p>3. Dodane zadnie można edytować, dodać datę, zmienić jego pozycję na liście lub usunąć.</p>
+    <p>4. W prawym górnym rogu naciskając na lupę możemy filtrować nasze zadania</p>
   </div>
 </template>
