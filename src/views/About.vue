fd666c7605cb6ffd7913cb8f58deb3e26e08f46f
Jacek <ja_cek@gmx.com>
2021-06-01 01:38:47 +0200

    project update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about pa-6">
     <h1>{{ $store.state.appTitle }}</h1>
-    <p>Jest to bardzo prosta i darmowa aplikacja do zarządzania swoimi zadaniami.Ogarnij się pomoże Ci w organizacji jakichkolwiek zadań, nieważne kim jesteś, ani co robisz! Rozpocznij od razu, szybko i łatwo dodając nowe zadania</p>
+    <p>Jest to bardzo prosta i darmowa aplikacja do zarządzania swoimi zadaniami.Ogarnij się pomoże Ci w organizacji, nieważne kim jesteś, ani co robisz! Rozpocznij od razu, szybko i łatwo dodając nowe zadania</p>
     <h3>Instrukcja obsługi</h3>
     <p>1. Aby dodać pierwszą i kolejne pozycje naciśnij pod datą na pole z napisem dodaj zadanie.</p>
     <p>2. Następnie wpisz zadanie i potwierdź znakiem + lub na klawiaturze klawiszem enter/return.</p>
